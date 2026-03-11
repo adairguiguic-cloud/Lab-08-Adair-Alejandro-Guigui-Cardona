c2271ab50a8e1aa2f4145ea5cd8db52e8371fd30
espaesix-tech <espaesix@gmail.com>
2026-03-11 09:06:50 -0600

    xd

---
+++ lAB08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AnalisisNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = 0, n, p; int a = 0, r = 0;
+            Console.WriteLine("--- Analisis de Notas de 10 Estudiantes ---");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Ingrese la nota " + i + ": ");
+                if (double.TryParse(Console.ReadLine(), out n))
+                {
+                    s += n;
+                    if (n >= 61) { a++; } else { r++; }
+                }
+                else { s += 0; r++; }
+            }
+            p = s / 10;
+            Console.WriteLine("Promedio: " + p.ToString("F2"));
+            Console.WriteLine("Aprobados: " + a);
+            Console.WriteLine("Reprobados: " + r);
+            Console.WriteLine("Presione una tecla...");
+            Console.ReadKey();
+        }
+    }
+}
